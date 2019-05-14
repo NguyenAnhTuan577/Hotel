da5c5710a976c5ea9048b69e37ebb4ca7da12d47
Nguyễn Thanh Tuấn <tuanpenspinner@gmail.com>
2019-05-14 18:15:42 +0700

    Giao diện phần quản lý user

---
+++ app/src/main/java/com/example/hotel/Change_Language.java
@@ -0,0 +1,57 @@
+package com.example.hotel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class Change_Language extends AppCompatActivity {
+    ListView listViewLanguage;
+    ArrayList<String> arrLanguage;
+    TextView txtBackLanguage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change__language);
+
+        listViewLanguage=(ListView) findViewById(R.id.listViewLanguage);
+        txtBackLanguage=(TextView) findViewById(R.id.txtBackLanguage);
+
+        txtBackLanguage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        InitArrayLanguage();
+        ArrayAdapter adapter=new ArrayAdapter(Change_Language.this,R.layout.support_simple_spinner_dropdown_item,arrLanguage);
+        listViewLanguage.setAdapter(adapter);
+    }
+
+    public void InitArrayLanguage()
+    {
+        arrLanguage=new ArrayList<>();
+        arrLanguage.add("Bengali");
+        arrLanguage.add("Bhojpuri");
+        arrLanguage.add("English");
+        arrLanguage.add("German");
+        arrLanguage.add("Hausa");
+        arrLanguage.add("Italian");
+        arrLanguage.add("Japanese");
+        arrLanguage.add("Korean");
+        arrLanguage.add("Mandarin Chinese");
+        arrLanguage.add("Persian");
+        arrLanguage.add("Portuguese");
+        arrLanguage.add("Thai");
+        arrLanguage.add("Vietnamese");
+        arrLanguage.add("Yue Chinese ");
+        arrLanguage.add("Western Punjabi");
+    }
+}
--- app/src/main/java/com/example/hotel/UserFragment.java
+++ app/src/main/java/com/example/hotel/UserFragment.java
@@ -1,17 +1,99 @@
 package com.example.hotel;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class UserFragment extends Fragment {
+public class UserFragment extends Fragment implements View.OnClickListener {
+    Button txtLanguage,txtHistory,txtInforCard,txtNotify,txtSecure,txtHelp,txtLogout;
+    ImageView imgEditInfor;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_user,container,false);
+        View view=inflater.inflate(R.layout.fragment_user,container,false);
+
+        imgEditInfor=(ImageView) view.findViewById(R.id.imgEditInfor);
+        txtLanguage=(Button) view.findViewById(R.id.txtLanguage);
+        txtHistory=(Button) view.findViewById(R.id.txtHistory);
+        txtInforCard=(Button) view.findViewById(R.id.txtInforCard);
+        txtNotify=(Button) view.findViewById(R.id.txtNotify);
+        txtSecure=(Button) view.findViewById(R.id.txtSecure);
+        txtHelp=(Button) view.findViewById(R.id.txtHelp);
+        txtLogout=(Button) view.findViewById(R.id.txtLogout);
+
+        txtLanguage.setOnClickListener((OnClickListener) this);
+        txtHistory.setOnClickListener((OnClickListener) this);
+        txtInforCard.setOnClickListener((OnClickListener) this);
+        txtNotify.setOnClickListener((OnClickListener) this);
+        txtSecure.setOnClickListener((OnClickListener) this);
+        txtHelp.setOnClickListener((OnClickListener) this);
+        txtLogout.setOnClickListener((OnClickListener) this);
+        imgEditInfor.setOnClickListener((OnClickListener) this);
+
+        return view;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case  R.id.imgEditInfor: {
+                Intent intent=new Intent(getContext(),Change_Infor.class);
+                startActivity(intent);
+                break;
+            }
+
+            case  R.id.txtLanguage: {
+                Intent intent=new Intent(getContext(),Change_Language.class);
+                startActivity(intent);
+                break;
+            }
+
+            case R.id.txtHistory: {
+                Intent intent=new Intent(getContext(),History.class);
+                startActivity(intent);
+                break;
+            }
+
+            case  R.id.txtInforCard: {
+                Intent intent=new Intent(getContext(),InforCard.class);
+                startActivity(intent);
+                break;
+            }
+
+            case R.id.txtNotify: {
+                Intent intent=new Intent(getContext(),Notify.class);
+                startActivity(intent);
+                break;
+            }
+            case  R.id.txtSecure: {
+                Intent intent=new Intent(getContext(),SecurAccount.class);
+                startActivity(intent);
+                break;
+            }
+
+            case R.id.txtHelp: {
+                Intent intent=new Intent(getContext(),Help.class);
+                startActivity(intent);
+                break;
+            }
+            case  R.id.txtLogout: {
+                break;
+            }
+
+            default:{
+                break;
+            }
+        }
     }
 }
