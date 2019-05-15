e3289f0a6008a426507ff94ba6daec906fba19aa
Nguyễn Thanh Tuấn <tuanpenspinner@gmail.com>
2019-05-15 13:57:07 +0700

    phục hồi file

---
+++ app/src/main/java/com/example/hotel/ui/main/FavoriteFragment.java
@@ -0,0 +1,19 @@
+package com.example.hotel.ui.main;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hotel.R;
+
+public class FavoriteFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_favorites,container,false);
+    }
+}
